20ff6ac159b35a33fa7aa95d4bd4049ecb4d81e1
lionng <154704230@qq.com>
2021-04-22 00:33:54 +0800

    spring boot admin init

---
+++ spring-boot-admin-client-one/src/main/java/top/zhangxiaofeng/SpringBootAdminClientOneApplication.java
@@ -0,0 +1,13 @@
+package top.zhangxiaofeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminClientOneApplication.class, args);
+    }
+
+}
